b7ab753c78b696aa6a53efed1716e4bff87144bb
Max <Kick52@yandex.ru>
2022-06-10 13:44:59 +0300

    lesson one

---
+++ src/lesson1/homework_1.java
@@ -0,0 +1,45 @@
+package lesson1;
+
+import java.util.Locale;
+
+/////////////////task_1////////////////////////
+
+//public class homework_1 {
+//     public static void main(String[] args) {
+//        String hi = "                Hello ";
+//        String world = " WoRld!";
+//        char newLine = '\n';
+//         String new_str = (hi.trim())+(world.toLowerCase(Locale.ROOT))+(newLine);
+//         System.out.print(new_str.repeat(3));
+//    }
+//}
+
+/////////////////task_2////////////////////////
+
+//public class homework_1 {
+//    public static void main(String[] args) {
+//     float weight = (float) 75.6;
+//     float height = (float) 1.75;
+//
+//     float mass_body = (weight/height);
+//     System.out.print(mass_body);
+//    }
+//}
+
+/////////////////task_3////////////////////////
+
+public class homework_1 {
+    public static void main(String[] args) {
+        String [] array = new String[6];
+        array[0] = "a";
+        array[1] = "b";
+        array[2] = "c";
+        array[3] = "d";
+        array[4] = "e";
+        array[5]= "h";
+        System.out.print(array[0]+array[1]+array[2]+array[3]+array[4]+ "\n" +array[0]+array[1]+array[2]+array[5]+array[4]);
+
+
+
+    }
+}
